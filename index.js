0466265ac6d44926300b1364475416468202922b
karin yahud <karin167bz@gmail.com>
2021-04-14 10:56:32 +0200

    karin-task

---
+++ index.js
@@ -0,0 +1,9 @@
+//1
+//if the first number more big from the seconde number print it 
+//if the seconde number more biger print it
+
+//2
+//if the first and the second number equal print true otherwies print false
+
+//3
+//only if the first number more big than seconde number print true, otherwies print false
